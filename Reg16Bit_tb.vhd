641a0dd6b7ee2e65c35a12da0d9fdff58c5a0a4e
joaopedrocastilhocardoso <95507750+joaopedrocastilhocardoso@users.noreply.github.com>
2024-05-10 19:48:21 -0300

    Update Reg16Bit_tb.vhd
    Updated code to english

--- Reg16Bit_tb.vhd
+++ Reg16Bit_tb.vhd
@@ -5,21 +5,25 @@ use ieee.numeric_std.all;
 entity Reg16Bit_tb is
 end entity;
 
-architecture teste of Reg16Bit_tb is
+architecture test of Reg16Bit_tb is
 	component Reg16Bit
 		port(
-		clk     : in std_logic;
-		wrEn    : in std_logic;
-		rst     : in std_logic;
+		clk     : in std_logic;  --Clock signal
+		wrEn    : in std_logic;  --Write enable pin
+		rst     : in std_logic;  --Reset pin
 		dataIn  : in unsigned (15 downto 0);
 		dataOut : out unsigned (15 downto 0)
 		);
 	end component;
+
+	--Testbench signal declarations
 	
 	signal tb_clk, tb_wrEn, tb_rst : std_logic;
 	signal tb_dataIn, tb_dataOut   : unsigned (15 downto 0);
 	signal finished                : std_logic := '0';
-	
+
+	--Constants for testbench declarations
+
 	constant period_time           : time      := 100 ns;
 	constant VCC                   : unsigned (15 downto 0) := "1111111111111111";
 	constant GND                   : unsigned (15 downto 0) := "0000000000000000";
@@ -37,7 +41,7 @@ architecture teste of Reg16Bit_tb is
 			dataOut => tb_dataOut
 		);
 			
-		reset_global : process begin -- sinal de reset
+		reset_global : process begin -- Reset signal
 		
 			tb_rst <= '1';
 			wait for period_time*2;
@@ -45,13 +49,13 @@ architecture teste of Reg16Bit_tb is
 			wait;
 		end process reset_global;
 		
-		sim_time_proc : process begin -- tempo de simulação
+		sim_time_proc : process begin -- Total simulation time
 			wait for 10 us;
 			finished <= '1';
 			wait;
 		end process sim_time_proc;
 		
-		clk_proc : process begin -- sinal de clock
+		clk_proc : process begin -- Clock signal
 		
 			while finished /= '1' loop
 				tb_clk <= '0';
@@ -62,7 +66,7 @@ architecture teste of Reg16Bit_tb is
 			wait;
 		end process clk_proc;
 		
-		testing_process : process begin -- linha de comandos para testar o registrador
+		testing_process : process begin -- Command line for testing
 			wait for period_time*2;
 			tb_wrEn <= '0';
 			tb_dataIn <= VCC;
@@ -83,4 +87,4 @@ architecture teste of Reg16Bit_tb is
 			tb_rst <= '0';
 			wait;
 		end process testing_process;
-	end architecture teste;
+	end architecture test;
