e4c9fbcff9fa0f2d6ebc347aac4cc0c4b81e51f2
joaopedrocastilhocardoso <95507750+joaopedrocastilhocardoso@users.noreply.github.com>
2024-05-14 12:10:45 -0300

    Add register bank files
    The register bank is a fundamental building block for the processor, our register bank will be composed of 8 2 byte registers, being a 16 byte memory

---
+++ RegBank.vhd
@@ -0,0 +1,62 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity RegBank is
+	port(
+		clk, rst, wrEn : in std_logic;
+		regCode        : in unsigned(2 downto 0);
+		dataIn         : in unsigned(15 downto 0); 
+		dataOut        : out unsigned(15 downto 0)
+	);
+	end entity RegBank;
+	
+	architecture a_RegBank of RegBank is
+	
+		component Reg16Bit is
+			port(
+				clk, rst, wrEn : in std_logic;
+				dataIn         : in unsigned (15 downto 0);
+				dataOut        : out unsigned (15 downto 0)
+			);
+		end component;
+		-- signal declaration for all registers 0 to 7
+		signal dataOut0, dataOut1, dataOut2, dataOut3, dataOut4, dataOut5, dataOut6, dataOut7 : unsigned (15 downto 0);
+		signal wrEn0, wrEn1, wrEn2, wrEn3, wrEn4, wrEn5, wrEn6, wrEn7                         : std_logic;
+		
+		begin
+		-- MUX for register wrEn selection
+		
+		wrEn0 <= '1' when regCode = "000" and wrEn = '1' else '0';
+		wrEn1 <= '1' when regCode = "001" and wrEn = '1' else '0';
+		wrEn2 <= '1' when regCode = "010" and wrEn = '1' else '0';
+		wrEn3 <= '1' when regCode = "011" and wrEn = '1' else '0';
+		wrEn4 <= '1' when regCode = "100" and wrEn = '1' else '0';
+		wrEn5 <= '1' when regCode = "101" and wrEn = '1' else '0';
+		wrEn6 <= '1' when regCode = "110" and wrEn = '1' else '0';
+		wrEn7 <= '1' when regCode = "111" and wrEn = '1' else '0';
+		
+		-- Register declaration
+		
+		reg0 : Reg16Bit port map(clk, rst, wrEn0, dataIn, dataOut0);
+		reg1 : Reg16Bit port map(clk, rst, wrEn1, dataIn, dataOut1);
+		reg2 : Reg16Bit port map(clk, rst, wrEn2, dataIn, dataOut2);
+		reg3 : Reg16Bit port map(clk, rst, wrEn3, dataIn, dataOut3);
+		reg4 : Reg16Bit port map(clk, rst, wrEn4, dataIn, dataOut4);
+		reg5 : Reg16Bit port map(clk, rst, wrEn5, dataIn, dataOut5);
+		reg6 : Reg16Bit port map(clk, rst, wrEn6, dataIn, dataOut6);
+		reg7 : Reg16Bit port map(clk, rst, wrEn7, dataIn, dataOut7);
+		
+		-- MUX for register dataOut selection
+		
+		dataOut <= dataOut0 when regCode = "000" else
+		dataOut1 when regCode = "001" else
+		dataOut2 when regCode = "010" else
+		dataOut3 when regCode = "011" else
+		dataOut4 when regCode = "100" else
+		dataOut5 when regCode = "101" else
+		dataOut6 when regCode = "110" else
+		dataOut7 when regCode = "111" else 
+		"0000000000000000";
+		
+	end architecture a_RegBank;
---
+++ RegBank_tb.vhd
@@ -0,0 +1,89 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity RegBank_tb is
+end entity; 
+
+architecture teste of RegBank_tb is
+	component RegBank is
+		port(
+			clk, rst, wrEn : in std_logic;
+			regCode        : in unsigned(2 downto 0);
+			dataIn         : in unsigned(15 downto 0); 
+			dataOut        : out unsigned(15 downto 0)
+		);
+	end component;
+	signal clk_tb, rst_tb          : std_logic;
+	signal wrEn_tb                 : std_logic := '0';
+	signal finished                : std_logic := '0';
+	signal regCode_tb              : unsigned (2 downto 0) := "000";
+	signal dataIn_tb, dataOut_tb   : unsigned (15 downto 0);
+		
+	-- Constants
+		
+	constant period_time : time := 100 ns;
+	constant X           : unsigned(15 downto 0) := "1010101010101010";
+	constant Y           : unsigned(15 downto 0) := "1100110011001100";
+	constant Z           : unsigned(15 downto 0) := "1111000011110000";
+ 		
+	begin
+		
+	bank1 : RegBank 
+	port map(
+		clk     => clk_tb,
+		rst     => rst_tb,
+		wrEn    => wrEn_tb,
+		regCode => regCode_tb,
+		dataIn  => dataIn_tb,
+		dataOut => dataOut_tb
+	);
+				
+	sim_time_proc :  process begin -- tempo de simulação
+				
+		wait for 10 us;
+		finished <= '1';
+		wait;
+	end process sim_time_proc; 
+			
+	clk_proc : process begin -- sinal de clock					
+			while finished /= '1' loop
+			clk_tb <= '0';
+			wait for period_time/2;
+			clk_tb <= '1';
+			wait for period_time/2;
+		end loop;
+		wait;
+	end process clk_proc;
+			
+	reset_global : process begin -- sinal de reset_global
+		rst_tb <= '1';
+		wait for period_time*2;
+		rst_tb <= '0';
+		wait;
+	end process reset_global;
+				
+	testing_process : process begin -- linha de comandos para testar o banco de registradores
+		wait for period_time*2;
+		regCode_tb <= "101";
+		dataIn_tb <= X;
+		wait for period_time*2;
+		wrEn_tb <= '1';
+		wait for period_time*2;
+		dataIn_tb <= Y;
+		wait for period_time*2;
+		regCode_tb <= "000";
+		wait for period_time*2;
+		dataIn_tb <= Z;
+		wrEn_tb <= '0';
+		regCode_tb <= "111";
+		wait for period_time*2;
+		wrEn_tb <= '1';
+		wait for period_time*2;
+		regCode_tb <= "001";
+		wait;
+	end process testing_process;
+end architecture teste;			
+					
+					
+			
