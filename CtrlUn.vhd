098c875ccf7a6c8a3ef2d625c05e138ea0967ec8
joaopedrocastilhocardoso <95507750+joaopedrocastilhocardoso@users.noreply.github.com>
2024-08-16 15:06:20 -0300

    Update CtrlUn.vhd
    Updated control unit to work with final version of the project

--- CtrlUn.vhd
+++ CtrlUn.vhd
@@ -4,7 +4,14 @@ use ieee.numeric_std.all;
 
 entity CtrlUn is
 	port(
-		clk, rst : in std_logic
+		clk, rst, ULAZero, ULANegative : in std_logic;
+		ULAOpCode, BRegCode            : out unsigned(2 downto 0);
+		ConstantToUla                  : out unsigned(11 downto 0);
+		BWrEn, AccWrEn                 : out std_logic;
+		MUXBank, MUXUla, MuxAcc        : out std_logic;
+		AccRst                         : out std_logic;
+		ULAFlagsWrEn                   : out std_logic;
+		RAMWrEnOut                     : out std_logic
 	);
 	end entity CtrlUn;
 	
@@ -12,9 +19,10 @@ entity CtrlUn is
 	
 		component PCcomplete is 
 		port(
-			clk, wrEn, rst, jumpInst : in std_logic;
-			dataIn                   : in unsigned(6 downto 0);
-			dataOut                  : out unsigned(6 downto 0)
+			clk, rst        : in std_logic;
+			state, jumpInst : in unsigned(1 downto 0);
+			dataIn          : in unsigned(6 downto 0);
+			dataOut         : out unsigned(6 downto 0)
 		);
 		end component;
 		
@@ -28,31 +36,103 @@ entity CtrlUn is
 		
 		component Decoder is 
 		port(
-			instruction : in unsigned(15 downto 0);
-			jumpInst    : out std_logic;
-			jumpAdrs    : out unsigned(6 downto 0)
+			instruction                  : in unsigned(15 downto 0);
+			flagULAZero, flagULANegative : in std_logic;
+			state                        : in unsigned(1 downto 0);
+			
+			jumpInst                     : out unsigned(1 downto 0);
+			jumpAdrs                     : out unsigned(6 downto 0);
+			
+			ULAOpCode                    : out unsigned(2 downto 0);
+			ConstantToUla                : out unsigned(11 downto 0);
+			BankWrEn, AccumulatorWrEn    : out std_logic;
+			BankRegCode                  : out unsigned(2 downto 0);
+			muxUlaIn, muxBankIn          : out std_logic;
+			rstAcc                       : out std_logic;
+			ULAFlagsWrEn                 : out std_logic;
+			RAMWrEn                      : out std_logic;
+			MuxAccIn                     : out std_logic
 		);
 		end component;
 		
-		component StateMachine1Bit is
+		component StateMachine2Bits is
 		port(
 			clk   : in std_logic;
 			rst   : in std_logic;
-			state : out std_logic
+			state : out unsigned(1 downto 0)
 		);
 		end component;
 		
-		signal WrEn, jumpInst, state : std_logic;
-		signal PCin, PCout           : unsigned(6 downto 0);
-		signal ROMOut                : unsigned(15 downto 0);
+		-- signal declarations
 		
+		signal jumpInst_s               : unsigned(1 downto 0) := "00";
+		signal state_s                  : unsigned(1 downto 0) := "00";
+		signal PCin_s, PCout_s          : unsigned(6 downto 0) := "0000000";
+		signal instruction_s            : unsigned(15 downto 0) := "0000000000000000";
+		signal BankRegCode_s            : unsigned(2 downto 0) := "000";
+		signal ConstantToUla_s          : unsigned (11 downto 0) := "000000000000";
+		signal BankWrEn_s, AccWrEn_s    : std_logic := '0';
+		signal muxUlaIn_s, muxBankIn_s  : std_logic := '0';
+		signal rstAcc_s, ULAFlagsWrEn_s : std_logic := '0';
+		signal ULAOpCode_s              : unsigned (2 downto 0) := "000";
+		signal RAMWrEn_s, MuxAccIn_s     : std_logic := '0';
 		begin
 		
 		--components declaration
 		
-		PC0   : PCcomplete port map(clk, state, rst, jumpInst, PCin, PCout);
-		ROM0  : ROM port map(clk, PCout, ROMOut);
-		Dec0  : Decoder port map (ROMOut, jumpInst, PCin);
-		StMach: StateMachine1Bit port map(clk, rst, state);
+		PC0  : PCcomplete port map(
+			clk      => clk, 
+			rst      => rst, 
+			jumpInst => jumpInst_s, 
+			state    => state_s, 
+			dataIn   => PCin_s, 
+			dataOut  => PCout_s
+			);
+		
+		ROM0 : ROM port map(
+			clk     => clk, 
+			address => PCout_s, 
+			output  => instruction_s
+		);
+		
+		Dec0 : Decoder port map(
+			instruction     => instruction_s, 
+			flagULAZero     => ULAZero, 
+			flagULANegative => ULANegative, 
+			state           => state_s, 
+			jumpInst        => jumpInst_s, 
+			jumpAdrs        => PCin_s, 
+			ULAOpCode       => ULAOpCode_s, 
+			ConstantToUla   => ConstantToUla_s, 
+			BankWrEn        => BankWrEn_s, 
+			AccumulatorWrEn => AccWrEn_s, 
+			BankRegCode     => BankRegCode_s, 
+			muxUlaIn        => muxUlaIn_s, 
+			muxBankIn       => muxBankIn_s, 
+			rstAcc          => rstAcc_s,
+			ULAFlagsWrEn    => ULAFlagsWrEn_s,
+			RAMWrEn         => RAMWrEn_s,
+			MuxAccIn        => MuxAccIn_s
+		);
+		
+		StMach: StateMachine2Bits port map(
+			clk   => clk, 
+			rst   => rst, 
+			state => state_s
+		);
+		
+		-- CPU outputs
+		
+		MuxAcc        <= MuxAccIn_s;
+		ConstantToUla <= ConstantToUla_s;
+		ULAOpCode     <= ULAOpCode_s;
+		BWrEn         <= BankWrEn_s;
+		AccWrEn       <= AccWrEn_s;
+		BRegCode      <= BankRegCode_s;
+		MUXUla        <= muxUlaIn_s;
+		MUXBank       <= muxBankIn_s;
+		AccRst        <= rstAcc_s;
+		ULAFlagsWrEn  <= ULAFlagsWrEn_s;
+		RAMWrEnOut    <= RAMWrEn_s;
 		
 	end architecture a_CtrlUn;
