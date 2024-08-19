5dcca78dfc967934b2f91182934e68fb15437161
joaopedrocastilhocardoso <95507750+joaopedrocastilhocardoso@users.noreply.github.com>
2024-08-19 14:22:51 -0300

    Update PCcomplete.vhd
    Updated PCcomplete file to work with the final version of this project

--- PCcomplete.vhd
+++ PCcomplete.vhd
@@ -4,9 +4,10 @@ use ieee.numeric_std.all;
 
 entity PCcomplete is
 	port(
-		clk, wrEn, rst, jumpInst : in std_logic;
-		dataIn                   : in unsigned(6 downto 0);
-		dataOut                  : out unsigned(6 downto 0)
+		clk, rst        : in std_logic;
+		state, jumpInst : in unsigned(1 downto 0);
+		dataIn          : in unsigned(6 downto 0);
+		dataOut         : out unsigned(6 downto 0)
 	);
 end entity PCcomplete;
 
@@ -24,13 +25,16 @@ architecture a_PCcomplete of PCcomplete is
 	
 	-- Declaração de sinais
 	signal PCOut, PCIn : unsigned(6 downto 0) := "0000000";	-- Endereços por padrão começam em 0
-
+	signal PCWrEn : std_logic := '0';
+	
 	begin
 	
-	PC0 : PC port map (clk, wrEn, rst, PCin, PCOut);
+	PCwrEn <= '1' when state = "00" else '0'; --Write enable on only during fetch phase, stage 00
+	
+	PC0 : PC port map (clk, PCwrEn, rst, PCin, PCOut);
 	
 	--MUX de entrada, para o próximo endereço caso tenha instrução de jump
-	PCIn <= PCOut + "0000001" when jumpInst = '0' else dataIn;
+	PCIn <= dataIn when jumpInst = "01" else PCOut + dataIn when jumpInst = "10" else PCOut + "0000001";
 	dataOut <= PCOut;
 	
 end a_PCcomplete;
